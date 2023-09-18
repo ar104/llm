29829d7e21eb9c74f88ede493a0f0ae6605dc6f8
Amitabha Roy <amitabha.roy@gmail.com>
2023-09-17 23:30:47 -0300

    Update glove.py
    Updates.

--- glove.py
+++ glove.py
@@ -5,7 +5,9 @@ import time
 start = time.time()
 word2idx = dict()  # map words to vector
 idx2word = []  # map vector back to word
-with open('C:/Users/aroym/Downloads/glove.6B/glove.6B.50d.txt',
+# Download vectors from here: https://nlp.stanford.edu/projects/glove/
+embeddings = 'glove.6B.50d.txt'
+with open(f'C:/Users/aroym/Downloads/glove.6B/{embeddings}',
           'r',
           encoding='utf-8') as f:
     vec_list = []
@@ -19,21 +21,17 @@ with open('C:/Users/aroym/Downloads/glove.6B/glove.6B.50d.txt',
     except:
         print(f'Hit exception when reading {line}')
     vectors = np.stack(vec_list, axis=0)
-print(f'Loaded vectors in {time.time() - start} seconds')
+print(f'Loaded {embeddings} vectors in {time.time() - start} seconds')
 king = vectors[word2idx['king']]
 queen = vectors[word2idx['queen']]
 man = vectors[word2idx['man']]
 woman = vectors[word2idx['woman']]
-# print(f'king = {king}')
-# print(f'queen = {queen}')
-# print(f'man = {man}')
-# print(f'woman = {woman}')
 query = king + (woman - man)
 query = np.expand_dims(query, 0)
 # Squared L2 dist.
 diff = vectors - query
 diff = np.sum(diff * diff, axis=1)
 closest = np.argsort(diff)
-print('king +  (woman - man) = ?')
+print('vec(king) +  (vec(woman) - vec(man)) = ? (L2 distance)')
 for i in range(10):
     print(f'{idx2word[closest[i]]}')
