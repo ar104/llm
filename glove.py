4c3c29e5fe88f5d972ba5e6c5c44e73379074080
Amitabha Roy <amitabha.roy@gmail.com>
2023-09-17 23:18:40 -0300

    Create glove.py

---
+++ glove.py
@@ -0,0 +1,39 @@
+import numpy as np
+import time
+
+
+start = time.time()
+word2idx = dict()  # map words to vector
+idx2word = []  # map vector back to word
+with open('C:/Users/aroym/Downloads/glove.6B/glove.6B.50d.txt',
+          'r',
+          encoding='utf-8') as f:
+    vec_list = []
+    try:
+        for line in f:
+            components = line.split()
+            word = components[0]
+            vec_list.append(np.asarray(components[1:], "float32"))
+            word2idx[word] = len(vec_list) - 1
+            idx2word.append(word)
+    except:
+        print(f'Hit exception when reading {line}')
+    vectors = np.stack(vec_list, axis=0)
+print(f'Loaded vectors in {time.time() - start} seconds')
+king = vectors[word2idx['king']]
+queen = vectors[word2idx['queen']]
+man = vectors[word2idx['man']]
+woman = vectors[word2idx['woman']]
+# print(f'king = {king}')
+# print(f'queen = {queen}')
+# print(f'man = {man}')
+# print(f'woman = {woman}')
+query = king + (woman - man)
+query = np.expand_dims(query, 0)
+# Squared L2 dist.
+diff = vectors - query
+diff = np.sum(diff * diff, axis=1)
+closest = np.argsort(diff)
+print('king +  (woman - man) = ?')
+for i in range(10):
+    print(f'{idx2word[closest[i]]}')
